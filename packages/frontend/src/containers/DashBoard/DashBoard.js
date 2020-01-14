7888e5380745a40b871febb3328d0933192014dd
hondahmd <mingda.hong@qq.com>
2020-01-14 18:17:03 +0800

    logout

--- packages/frontend/src/containers/DashBoard/DashBoard.js
+++ packages/frontend/src/containers/DashBoard/DashBoard.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import { Button } from '@material-ui/core';
+import PropTypes from 'prop-types';
 
 import server from 'constants/server';
 
-const DashBoard = userInfo => {
+const DashBoard = ({ userInfo, history }) => {
   const defaultFetch = 'Waiting for fetch data';
   const [fetchData, setFetchData] = useState(defaultFetch);
 
@@ -20,12 +21,17 @@ const DashBoard = userInfo => {
     setFetchData(data.message);
   }
 
+  function logOut() {
+    document.cookie = 'token=;expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
+    history.push('/');
+  }
+
   return (
     <div>
       <div>{userInfo.userInfo.userInfo !== undefined && userInfo.userInfo.userInfo.email}</div>
-      <Link to="/">
-        <Button variant="contained">Log Out</Button>
-      </Link>
+      <Button variant="contained" onClick={() => logOut()}>
+        Log Out
+      </Button>
       <Button variant="contained" onClick={() => handleClick()}>
         Request
       </Button>
@@ -34,8 +40,25 @@ const DashBoard = userInfo => {
   );
 };
 
+DashBoard.defaultProps = {
+  userInfo: {},
+  history: {}
+};
+
+DashBoard.propTypes = {
+  userInfo: PropTypes.shape({
+    userInfo: {
+      userInfo: PropTypes.string,
+      email: PropTypes.string
+    }
+  }),
+  history: {
+    push: PropTypes.func
+  }
+};
+
 const mapStateToProps = ({ userInfo }) => ({
   userInfo
 });
 
-export default connect(mapStateToProps, null)(DashBoard);
+export default connect(mapStateToProps, null)(withRouter(DashBoard));
